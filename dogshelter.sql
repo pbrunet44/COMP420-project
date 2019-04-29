64ae1508dd7758f31609a907687ebeaf5b8fa052
Unknown <darius.jones1018@gmail.com>
2019-04-29 14:32:18 -0700

    created skeletons for the tables

--- dogshelter.sql
+++ dogshelter.sql
@@ -1,7 +1,72 @@
-CREATE DATABASE dogshelter;
-USE dogshelter;
+create database dogshelter;
+use dogshelter;
 
-CREATE TABLE SHELTER(
+drop table if exists dog;
+create table dog(
+
+)engine=innodb;
+
+drop table if exists breed;
+create table breed(
 
 
-) ENGINE = INNODB;
+)engine=innodb;
+
+
+drop table if exists employees;
+create table employees(
+
+)engine=innodb;
+
+
+drop table if exists staff;
+create table staff(
+
+)engine=innodb;
+
+
+drop table if exists volunteers;
+create table volunteers(
+
+)engine=innodb;
+
+
+drop table if exists veterinarians ;
+create table veterinarians (
+
+)engine=innodb;
+
+drop table if exists pharmacy;
+create table pharmacy(
+
+)engine=innodb;
+
+drop table if exists drugs;
+create table drugs(
+
+)engine=innodb;
+
+
+drop table if exists adoptees;
+create table adoptees(
+
+)engine=innodb;
+
+
+drop table if exists foster;
+create table foster(
+
+)engine=innodb;
+
+drop table if exists adoption_records;
+create table adoption_records(
+
+)engine=innodb;
+
+drop table if exists health_record;
+create table health_record(
+
+)engine=innodb;
+
+
+
