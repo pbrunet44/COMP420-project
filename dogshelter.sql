3271439e1424e9dd901caa255a578f35a0def140
Unknown <darius.jones1018@gmail.com>
2019-04-30 09:12:33 -0700

    started creating tables

--- dogshelter.sql
+++ dogshelter.sql
@@ -15,4 +15,23 @@ drop table if exists adoption_cert; 		#11
 drop table if exists favorites_list;		#12
 
 
+create table shelter(
+sh_id int(11),
+sh_name varchar(35) unique not null,
+sh_addr varchar(50) unique not null,
+sh_phone varchar(10) unique not null,
+primary key (sh_id)
+)engine=innoDB;
 
+create table employee(
+em_id int(11),
+sh_id int(11),
+em_fname varchar(30),
+em_hireDate Date not null,
+primary key (em_id, sh_id),
+foreign key (sh_id) references shelter(sh_id)
+)engine=innoDB;
+
+create table job_tile(
+job_code int(11)
+)engine=innoDB;
